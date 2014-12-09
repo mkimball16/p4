095737dcceeb1e7c7d5ddfd886254706a0046f36
MacKenzie Kimball <mackenziekimball@gmail.com>
2014-12-08 20:47:02 -0500

    Changed text on index page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,8 +10,9 @@
 
 @section('content')
 
-<h1>Show users events here</h1>
+<h1>Welcome to Parties On Pennies <br>Event Management System</h1>
+<p>Use this tool to track your upcoming events. Add events, edits events, and track RSVPs from your guests</p>
 
-	{{ Form::close() }}
+	
 
 @stop
